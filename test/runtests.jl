f388eef1e161c3627d651168d3448610426a1a92
justyniaczek <44835858+justyniaczek@users.noreply.github.com>
2020-05-15 21:40:20 +0200

    test Appvayor

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,7 +2,7 @@ using mopkg
 using Test
 
 @testset "mopkg.jl" begin
-    @test example_fun(2) == 4
-    @test example_fun(-2) == 4
+    @test example_func(2) == 4
+    @test example_func(-2) == 4
     # Write your own tests here.
 end
