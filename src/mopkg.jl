b6d85df0bdce47fb21b55cae40026ce7556dbb79
justyniaczek <44835858+justyniaczek@users.noreply.github.com>
2020-05-15 22:02:12 +0200

    Update mopkg.jl

--- src/mopkg.jl
+++ src/mopkg.jl
@@ -1,7 +1,40 @@
 module mopkg
 
-example_func(x) = x^2
+function powell(f, x1, x2, x3)
+    fx1 = f(x1)
+    fx2 = f(x2)
+    fx3 = f(x3)
+    i = 1
+    while x3 - x1 > 0.165
 
-export example_func
+        global x4 = 0.5 * ((x2^2 - x3^2) * fx1 + (x3^2 - x1^2)* fx2 + (x1^2 - x2^2) * fx3) /( (x2 - x3)*fx1 + (x3 - x1)* fx2 +(x1 - x2) * fx3)
+        println("fx1: ", fx1, " fx2: ", fx2, " fx3: ", fx3, " x4: ", x4, " i: ", i)
+        fx4 = f(x4)
+        i= i + 1
+
+        if x4 > x2
+            if fx4 < fx2
+                x1 = x2
+                x2 = x4
+                fx1 = fx2
+                fx2 = fx4
+            else
+                x3 = x4
+                fx3 = fx4
+            end
+        elseif fx4 < fx2
+            x3 = x2
+            x2 = x4
+            fx3 = fx2
+            fx2 = fx4
+        else
+            x1 = x4
+            fx1 = fx4
+        end
+    end
+    println("Minimum: ", x4 , " counted at ", i - 1 , " iteration")
+end
+
+export powell
 
 end # module
