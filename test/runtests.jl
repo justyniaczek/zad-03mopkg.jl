463d8ff9bfbea5930b81887b263b3b75e91e502a
justyniaczek <44835858+justyniaczek@users.noreply.github.com>
2020-05-15 22:15:18 +0200

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,32 @@
 using mopkg
 using Test
 
-@testset "mopkg.jl" begin
-    @test powell(0) == 0
+using mopkg
+import mopkg: fdc, sfdc, find_min_interval
+using Test
+import InteractiveUtils: subtypes
+"""
+Single variable global test functions – functions with single global minimum
+key is function (lambda), value (y, x) in minimum
+"""
+svltf = Dict(
+    (x -> x^2 - 1) => (-1.0, 0.0),
+    (x -> x^2/3 + 2x - sin(x)) => (-3.423528818, -3.99083),
+	(x -> x^4-8) => (-8.0, 0.0),
+	(x -> x^2-6x+3) => (-6.0, 3.0))
 
-    # Write your own tests here.
-end
+@testset "Single variable optimizers" begin
+    @testset "General test for SVOptMethods" begin
+        for (fun, min) in svltf
+            tval = [min[2] - 2, min[2] + 3]
+            for stval in tval
+                @testset "Epsilon tests" begin
+                    for optim in subtypes(SVOptMethod)
+                        for tolerance in [1e-2, 1e-4, 1e-6]
+                            @test isapprox(line_optimize(fun, stval; eps=tolerance, method=optim())[1], min[1], atol=tolerance)
+                        end
+                    end
+                end
+            end
+        end
+    end
