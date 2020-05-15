1a8dd452c1845959e0e4f02e618a4ba66ca51536
justyniaczek <44835858+justyniaczek@users.noreply.github.com>
2020-05-16 00:44:13 +0200

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,5 +4,6 @@ using Test
 @testset "mopkg.jl" begin
     x1, x2, x3, f = -3.0, 3.0, 1.0, x -> x^4 + 3x^3 + x^2 + sin(x)
     @test powell(f, x1, x2 , x3) == (-1.971275676169661)
+        @test powell(f, x1, x2 , x3) == (-6)
     # Write your own tests here.
 end
